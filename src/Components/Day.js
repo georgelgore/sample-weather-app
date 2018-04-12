06dc99528ac26b72aea56b0b656eb872bec2bc80
George Gore <georgegore@Georges-MacBook-Pro.local>
2018-04-12 12:42:03 -0400

    toggle works for max, min, and displays correct C/F after temp

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -4,19 +4,19 @@ const Day = props => {
   console.log('IN DAY', props)
   return(
     <div className="ui card">
-      <div className="image">
-        <img src={`/icons/${props.details.icon}`} alt="test"/>
-      </div>
       <div className="content">
-        {props.details.dateTimeISO.slice(0,10)}
+        <h1>{props.details.dateTimeISO.slice(0,10)}</h1>
+      </div>
+      <div className="image">
+      <img src={`/icons/${props.details.icon}`} alt="test"/>
       </div>
       <div className="meta">
-        <p>
-          High: {props.details.maxTempF}
-        </p>
-        <p>
-          Low: {props.details.minTempF}
-        </p>
+        <div>
+          <h2> High:{!!props.fahrenheit ? `${props.details.maxTempF}°F` : `${props.details.maxTempC}°C`}</h2>
+        </div>
+        <div>
+          <h2> Low:{!!props.fahrenheit ? `${props.details.minTempF}°F` : `${props.details.minTempC}°C`}</h2>
+        </div>
       </div>
       <br/>
     </div>
