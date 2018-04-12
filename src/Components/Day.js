a1616a0236e197411e54ab43aaa2e099367a66ff
George Gore <georgegore@Georges-MacBook-Pro.local>
2018-04-12 12:24:54 -0400

    add icons

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Day = props => {
-  console.log('IN DAY', props.details)
+  console.log('IN DAY', props.details.icon)
   return(
     <div className="ui card">
+      <div className="image">
+        <img src="/Users/georgegore/dev/post/c4q/new/sample-weather-app/public/icons/am_showshowers.png" alt="test"/>
+      </div>
       <div className="content">
         {props.details.dateTimeISO.slice(0,10)}
       </div>
